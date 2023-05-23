05ddf660923164233705c4ec02c87d47c5fb6bf7
Tomas Zubik <zubik.tomas@gmail.com>
2023-05-23 14:59:45 -0300

    changed csv time format

--- dags/research_etl.py
+++ dags/research_etl.py
@@ -4,7 +4,7 @@ def run_research_etl():
     from spotify_top50_etl import extract,transform
     from custom_spotipy import sp
     top_50_playlist_id = '37i9dQZEVXbMMy2roB9myp'
-    now = datetime.today().strftime('%Y-%m-%d--%H:%Mhs')
+    now = datetime.today().strftime('%Y-%m-%d--%H-%M')
     
     raw = extract(top_50_playlist_id,sp)
     df = transform(raw)
